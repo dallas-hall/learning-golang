baca3941a4ba0a48dfaf8d79808e77a6fc91bb1d
Dallas Hall <mr.dallas.hall@gmail.com>
2026-06-07 18:42:58 +1000

    Udpated markdown headings.

--- chapter01/hello.go
+++ chapter01/hello.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("# Chapter 1")
+	fmt.Println("## Exercise 1")
 	fmt.Println("Hello, world!")
 	fmt.Printf("Hello, %s!\n", "again World")
 }
--- chapter02/exercises.go
+++ chapter02/exercises.go
@@ -6,14 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Println("# Exercise 1")
+	fmt.Println("# Chapter 2")
+	fmt.Println("## Exercise 1")
 	var i = 20
 	var f = float64(i)
 
 	fmt.Printf("i: %d\n", i)
 	fmt.Printf("f: %f\n", f)
 
-	fmt.Println("# Exercise 2")
+	fmt.Println("## Exercise 2")
 	const n = 10
 	i2 := n
 	var f2 float64 = n
@@ -21,9 +22,9 @@ func main() {
 	fmt.Printf("i2: %d\n", i2)
 	fmt.Printf("f2: %f\n", f2)
 
-	fmt.Println("# Exercise 3")
+	fmt.Println("## Exercise 3")
 
-	fmt.Println("## Max Values")
+	fmt.Println("### Max Values")
 	var b byte = math.MaxUint8
 	var smallInt int32 = math.MaxInt32
 	var bigInt uint64 = math.MaxInt64
@@ -31,7 +32,7 @@ func main() {
 	fmt.Printf("smallInt: %d\n", smallInt)
 	fmt.Printf("bigInt: %d\n", bigInt)
 
-	fmt.Println("## Max Values + 1")
+	fmt.Println("### Max Values + 1")
 	b += 1
 	smallInt += 1
 	bigInt += 1
