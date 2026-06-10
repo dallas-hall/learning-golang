b69d8e94e915cf6237435acd5a3c34ff3c07cd04
Dallas Hall <mr.dallas.hall@gmail.com>
2026-06-10 18:05:47 +1000

    Added comments to explain what is happening

--- chapter05/exercises.go
+++ chapter05/exercises.go
@@ -20,6 +20,8 @@ func div(i int, j int) (int, error) {
 	return i / j, nil
 }
 
+// A map where k = string and v = func
+// ie the "+" string will call the add function.
 var opMap = map[string]func(int, int) (int, error){
 	"+": add,
 	"-": sub,
@@ -29,14 +31,22 @@ var opMap = map[string]func(int, int) (int, error){
 
 // Exercise 2
 func fileLen(fileName string) (int, error) {
+	// Open the file
 	f, err := os.Open(fileName)
 	if err != nil {
 		return 0, err
 	}
+	// Close the file once the function has finished
 	defer f.Close()
+
+	// Create a buffer to read file data into
 	data := make([]byte, 2048)
+
+	// Keep a running total of bytes read
 	total := 0
+	// Read bytes into the buffer until an error (EOF or other)
 	for {
+		// This may not always fill the buffer, so keep a count
 		count, err := f.Read(data)
 		total += count
 		if err != nil {
@@ -50,7 +60,10 @@ func fileLen(fileName string) (int, error) {
 }
 
 // Exercise 3
+// Take a string and return a function
 func prefixer(prefix string) func(string) string {
+	// This is a closure because prefix is used from the other function
+	// This is an anonymous function because it has no name
 	return func(body string) string {
 		return prefix + " " + body
 	}
@@ -106,7 +119,9 @@ func main() {
 	fmt.Println(count)
 
 	fmt.Println("## Exercise 3")
+	// This is a closure because prefixer remembers "Hello" after it runs.
 	helloPrefix := prefixer("Hello")
+	// The closure is used here
 	fmt.Println(helloPrefix("Bob"))   // should print Hello Bob
 	fmt.Println(helloPrefix("Maria")) // should print Hello Maria
 }
