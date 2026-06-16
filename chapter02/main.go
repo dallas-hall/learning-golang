b2957e88411e35e67871e7c1d7c73f9b7798d396
Dallas Hall <mr.dallas.hall@gmail.com>
2026-06-16 11:03:46 +1000

    Chapter 2: types (eg structs), variables, functions.

---
+++ chapter02/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+}
+
+func printBook(b Book) {
+	// Print all arguments separated by spaces.
+	fmt.Println(b.Title, "by", b.Author, "-", b.Copies, "copies")
+}
+
+func printBook2(b Book) {
+	fmt.Printf("%v by %v - %v copies\n", b.Title, b.Author, b.Copies)
+}
+
+func main() {
+	fmt.Println("# Chapter 2")
+	book1 := Book{
+		Title:  "Sea Room",
+		Author: "Adom Nicolson",
+		Copies: 2,
+	}
+
+	printBook(book1)
+	printBook2(book1)
+}
