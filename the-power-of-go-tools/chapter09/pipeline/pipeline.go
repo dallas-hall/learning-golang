e15bfc89fab1cfe2bfff0bd932569aac9b45b120
Dallas Hall <mr.dallas.hall@gmail.com>
2026-07-15 18:32:47 +1000

    wip(Chapter 09): os.Open, bufio.NewScanner, strings.Fields, sort.Slice,         fluent API, method pipelines, bytes.Buffer, cmp.Equal, io.Writer,         io.Reader, errors.New, strings.NewReader, io.Copy, netip.Addr,         log.Fatalf, *regexp.Regexp.FindString, netip.ParseAddr, Test,         TestMain, testscript, io.ReadAll,

--- the-power-of-go-tools/chapter09/pipeline/pipeline.go
+++ the-power-of-go-tools/chapter09/pipeline/pipeline.go
@@ -1,8 +1,10 @@
 package pipeline
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
 )
@@ -23,7 +25,8 @@ func New() *Pipeline {
 	}
 }
 
-// This method got replaced by FromString, to remove a bug and use our new constructor.
+// This method got replaced by FromString. It removes a bug of no Output and
+// uses our new constructor.
 func FromStringOld(s string) *Pipeline {
 	return &Pipeline{
 		Input: strings.NewReader(s),
@@ -41,25 +44,67 @@ func FromString(s string) *Pipeline {
 // Create and return a Pipeline object with the Input set from a file.
 // The Output is Stdout. Error is nil.
 func FromFile(path string) *Pipeline {
-	p := New()
-	// Read file into something
 	file, err := os.Open(path)
 	if err != nil {
-		log.Printf("failed to open %q: %s", path, err)
 		return &Pipeline{Error: err}
 	}
-	// Don't use `defer file.Close()` because it will close the file on exiting this function.`
+	// Don't use this because it will close the file on exiting this function.`
+	// defer file.Close()
 
+	p := New()
 	p.Input = file
 	return p
 }
 
 // Copies the Input to the Output to form the "shell pipeline."
 // If the default hasn't been changed, this will be Stdout.
+// We always check for a Pipeline Error first and do nothing is one exists.
 func (p *Pipeline) Stdout() {
 	if p.Error != nil {
 		return
 	}
 	// Copies to io.Writer destination from io.Reader source.
 	io.Copy(p.Output, p.Input)
+}
+
+// Read everything from the Pipeline's input and return it as a string.
+// We always check for a Pipeline Error first and do nothing is one exists.
+func (p *Pipeline) String() (string, error) {
+	if p.Error != nil {
+		return "", p.Error
+	}
+
+	data, err := io.ReadAll(p.Input)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+// Read everything from the Pipeline's input, split the text using the delimiter
+// and return the supplied column from each line.
+// We always check for a Pipeline Error first and clear the reader if one exists.
+func (p *Pipeline) Column(column int, delimiter string) *Pipeline {
+	if p.Error != nil {
+		p.Input = strings.NewReader("")
+		return p
+	}
+	if column < 1 {
+		p.Error = fmt.Errorf("bad column, must be positive: %d\n", column)
+		return p
+	}
+
+	result := new(bytes.Buffer)
+	scanner := bufio.NewScanner(p.Input)
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), delimiter)
+		if len(fields) < column {
+			continue
+		}
+		fmt.Fprintln(result, fields[column-1])
+	}
+
+	return &Pipeline{
+		Input: result,
+	}
 }
--- the-power-of-go-tools/chapter09/pipeline/pipeline_test.go
+++ the-power-of-go-tools/chapter09/pipeline/pipeline_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestPipeline_StdoutPrintsToOutputFromString(t *testing.T) {
+func TestPipeline_StdoutPrintsStringToOutput(t *testing.T) {
 	t.Parallel()
 	want := "Helloworld 123\n"
 	p := pipeline.FromString(want)
@@ -26,11 +26,25 @@ func TestPipeline_StdoutPrintsToOutputFromString(t *testing.T) {
 	}
 }
 
-func TestPipeline_StdoutPrintsToOutputFromFile(t *testing.T) {
+func TestPipeline_StdoutPrintsNothingOnError(t *testing.T) {
+	t.Parallel()
+	want := "Helloworld 123\n"
+	p := pipeline.FromString(want)
+	buffer := new(bytes.Buffer)
+	p.Output = buffer
+	p.Error = errors.New("oops")
+	p.Stdout()
+	got := buffer.String()
+	if got != "" {
+		t.Errorf("want no output because of error & got %q", got)
+	}
+}
+
+func TestPipeline_FromFileReadsDataCorrectly(t *testing.T) {
 	t.Parallel()
 	want := []byte("Helloworld 123\n")
 
-	p := pipeline.FromFile("test/data/text.txt")
+	p := pipeline.FromFile("test/data/test.txt")
 	if p.Error != nil {
 		t.Fatal(p.Error)
 	}
@@ -44,16 +58,86 @@ func TestPipeline_StdoutPrintsToOutputFromFile(t *testing.T) {
 	}
 }
 
-func TestPipeline_StdoutFailsGracefullyOnPipelineError(t *testing.T) {
+func TestPipeline_FromFileSetsErrorGivenNonexistentFile(t *testing.T) {
+	t.Parallel()
+	p := pipeline.FromFile("doesnt-exist.txt")
+	if p.Error == nil {
+		t.Fatal("want error opening non-existent file, got nil")
+	}
+}
+
+func TestPipeline_StringReturnsPipeContentsAsString(t *testing.T) {
 	t.Parallel()
 	want := "Helloworld 123\n"
 	p := pipeline.FromString(want)
-	buffer := new(bytes.Buffer)
-	p.Output = buffer
-	p.Error = errors.New("oops")
-	p.Stdout()
-	got := buffer.String()
-	if got != "" {
-		t.Errorf("want no output because of error & got %q", got)
+	got, err := p.String()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Errorf("want %q & got %q", want, got)
+	}
+}
+
+func TestPipeline_StringReturnsErrorWhenPipelineErrorSet(t *testing.T) {
+	t.Parallel()
+	p := pipeline.FromString("Helloworld 123\n")
+	p.Error = errors.New("uh oh spaghettio, Pipeline error!")
+	_, err := p.String()
+	if err == nil {
+		t.Errorf("want Pipeline error from String but not nil")
+	}
+}
+
+func TestPipeline_ColumnGetsDataCorrectly(t *testing.T) {
+	t.Parallel()
+	want := "2\n2\n2\n"
+	p := pipeline.FromFile("test/data/test.csv")
+	if p.Error != nil {
+		t.Fatal(p.Error)
+	}
+
+	got, err := p.Column(2, ",").String()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Errorf("want %q & got %q", want, got)
+	}
+}
+
+func TestPipeline_ColumnProducesNothingWhenPipelineErrorSet(t *testing.T) {
+	t.Parallel()
+	p := pipeline.FromFile("test/data/test.csv")
+	p.Error = errors.New("uh oh spaghettio, Pipeline error!")
+	data, err := io.ReadAll(p.Column(2, ",").Input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(data) > 0 {
+		t.Errorf("want Pipeline error from Column but got %q", data)
+	}
+}
+
+func TestPipeline_ColumnProducesNothingWhenGivenInvalidColumn(t *testing.T) {
+	t.Parallel()
+	p := pipeline.FromFile("test/data/test.csv")
+	if p.Error != nil {
+		t.Fatal(p.Error)
+	}
+
+	p.Column(-1, ",")
+	if p.Error == nil {
+		t.Error("want Pipeline error from Column but got nil")
+	}
+
+	data, err := io.ReadAll(p.Column(1, ",").Input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(data) > 0 {
+		t.Errorf("want no output from Column but got %q", data)
 	}
 }
