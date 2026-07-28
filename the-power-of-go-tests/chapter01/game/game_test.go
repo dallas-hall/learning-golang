e6ca4fdeb82b1120ea175269b17cea348b2cb48d
Dallas Hall <mr.dallas.hall@gmail.com>
2026-07-28 11:05:31 +1000

    wip(chapter01): Add multiple tests cases and update function to handle them.

--- the-power-of-go-tests/chapter01/game/game_test.go
+++ the-power-of-go-tests/chapter01/game/game_test.go
@@ -7,20 +7,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_ListItemsGivesCorrectResultForInput(t *testing.T) {
-	t.Parallel()
-	input := []string{
-		"a battery",
-		"a key",
-		"a tourist map",
-	}
-	want := "You can see here a battery, a key, and a tourist map."
-	got := game.ListItems(input)
-	if want != got {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
+// This only works for items >= 2
 func Test_MyListItemsGivesCorrectResultForInput(t *testing.T) {
 	t.Parallel()
 	input := []string{
@@ -32,5 +19,48 @@ func Test_MyListItemsGivesCorrectResultForInput(t *testing.T) {
 	got := game.MyListItems(input)
 	if want != got {
 		t.Error(cmp.Diff(want, got))
+	}
+}
+
+// Instead of writing multiple tests for different inputs, we use one test that
+// loops over all the different inputs.
+func Test_ListItemsGivesCorrectResultForInput(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		input []string
+		want  string
+	}
+	cases := []testCase{
+		{
+			input: []string{},
+			want:  "",
+		},
+		{
+			input: []string{
+				"a battery",
+			},
+			want: "You can see a battery here.",
+		},
+		{
+			input: []string{
+				"a battery",
+				"a key",
+			},
+			want: "You can see here a battery and a key.",
+		},
+		{
+			input: []string{
+				"a battery",
+				"a key",
+				"a tourist map",
+			},
+			want: "You can see here a battery, a key, and a tourist map.",
+		},
+	}
+	for _, tc := range cases {
+		got := game.ListItems(tc.input)
+		if tc.want != got {
+			t.Error(cmp.Diff(tc.want, got))
+		}
 	}
 }
