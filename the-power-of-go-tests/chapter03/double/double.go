ba8f2891fb2595c2e854b03f19b4436e5bb123e5
Dallas Hall <mr.dallas.hall@gmail.com>
2026-07-31 18:23:25 +1000

    wip(chapter03): Go package Example code.

--- the-power-of-go-tests/chapter03/double/double.go
+++ the-power-of-go-tests/chapter03/double/double.go
@@ -1,8 +1,18 @@
 package double
 
+import "fmt"
+
+type Number struct {
+	Value int
+}
+
+func (n Number) ValueString() string {
+	return fmt.Sprintf("%d", n.Value)
+}
+
 func Double(n int) int {
 	// Add a temporary change to the function to skew the test result, this will
 	// check if the test is actually detecting bugs in the function. eg:
 	// return n * 2 + 1
-	return n*2 + 1
+	return n * 2
 }
--- the-power-of-go-tests/chapter03/double/double_test.go
+++ the-power-of-go-tests/chapter03/double/double_test.go
@@ -2,6 +2,7 @@ package double_test
 
 import (
 	"double"
+	"fmt"
 	"testing"
 )
 
@@ -13,4 +14,56 @@ func Test_Double2Returns4(t *testing.T) {
 	if want != got {
 		t.Errorf("want %d & got %d", want, got)
 	}
+}
+
+// This function gets included in the package documentation on pkg.go.dev and
+// it will be available to run in a sandbox.
+//
+// The `// Output:` has to be correct or this will fail when running `go test`.
+//
+// You can also use `// Unordered output:` for printing things like a map, where
+// the output is not deterministic.
+//
+// The function after Example must exist, as that is the function used by the
+// documentation tool.
+func ExampleDouble() {
+	fmt.Println(double.Double(2))
+	// Output:
+	// 4
+}
+
+// You can give multiple examples of the same function, add a suffix after the
+// function name.
+func ExampleDouble_with2() {
+	fmt.Println(double.Double(2))
+	// Output:
+	// 4
+}
+
+func ExampleDouble_with3() {
+	fmt.Println(double.Double(3))
+	// Output:
+	// 6
+}
+
+// This demonstrates how to use the entire package
+func Example() {
+
+}
+
+// Examples work with types as well
+func ExampleNumber() {
+	n := double.Number{Value: 2}
+	fmt.Println(n)
+	// Output:
+	// {2}
+}
+
+// Examples work on methods on types. The syntax is:
+// Example + Type Name + underscore + method name
+func ExampleNumber_ValueString() {
+	n := double.Number{Value: 2}
+	fmt.Println(n.ValueString())
+	// Output:
+	// 2
 }
