09746c133569f87def71e90f7046924c9a87d7d4
Dallas Hall <mr.dallas.hall@gmail.com>
2026-08-08 12:00:22 +1000

    task(chapter06): Generating random inputs with rand.IntN

--- the-power-of-go-tests/chapter06/codec/codec_test.go
+++ the-power-of-go-tests/chapter06/codec/codec_test.go
@@ -8,6 +8,7 @@ import (
 
 func Test_EncodeFollowedByDecodeGivesStartingValue(t *testing.T) {
 	t.Parallel()
+	// Create a random number between 1 and 10 (0 to 9 + 1)
 	input := rand.IntN(10) + 1
 	want := input
 	encoded := codec.Encode(input)
