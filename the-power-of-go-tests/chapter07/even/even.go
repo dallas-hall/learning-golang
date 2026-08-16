a2adb951e5d8c9006fa11c0dd697315f92bf72c2
Dallas Hall <mr.dallas.hall@gmail.com>
2026-08-16 12:19:35 +1000

    task(chapter07): Mutation testing with go-mutesting

--- the-power-of-go-tests/chapter07/even/even.go
+++ the-power-of-go-tests/chapter07/even/even.go
@@ -3,6 +3,8 @@ package even
 // We are using this function to showcase automated mutation testing, so we
 // aren't using `return n%2 == 0` so the mutation tester has enough code to
 // work with.
+// Install dependency with `go install github.com/avito-tech/go-mutesting/cmd/go-mutesting@latest`
+// Run with `go-mutesting .`
 func IsEven(n int) bool {
 	if n%2 == 0 {
 		return true
