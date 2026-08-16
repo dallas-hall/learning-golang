9ec37b8d479a537d97247db12c6a55ce4c615a04
Dallas Hall <mr.dallas.hall@gmail.com>
2026-08-16 12:41:33 +1000

    task(chapter07): Mutation testing with go-mutesting

--- the-power-of-go-tests/chapter07/even/even.go
+++ the-power-of-go-tests/chapter07/even/even.go
@@ -1,5 +1,13 @@
 package even
 
+import "sync"
+
+// Store previous calls to IsEvenCached into a memory cache.
+var (
+	mutex = sync.Mutex{}
+	cache = map[int]bool{}
+)
+
 // We are using this function to showcase automated mutation testing, so we
 // aren't using `return n%2 == 0` so the mutation tester has enough code to
 // work with.
@@ -12,4 +20,19 @@ func IsEven(n int) bool {
 		return true
 	}
 	return false
+}
+
+func IsEvenCache(n int) (even bool) {
+	// Need a mutex lock so t.Parrallel() works correctly
+	mutex.Lock()
+	defer mutex.Unlock()
+	even = n%2 == 0
+	if _, ok := cache[n]; !ok {
+		cache[n] = n%2 == 0
+		// This even is redundant, it is here so the mutation testing can show us
+		// it is redundant. ie there will be failing mutation tests showing the
+		// first or second even assignment statements being removed.
+		even = n%2 == 0
+	}
+	return even
 }
--- the-power-of-go-tests/chapter07/even/even_test.go
+++ the-power-of-go-tests/chapter07/even/even_test.go
@@ -27,3 +27,25 @@ func TestIsEvenReturningFalseForOddNumbers(t *testing.T) {
 		})
 	}
 }
+
+func TestIsEvenCacheReturningTrueForEvenNumbers(t *testing.T) {
+	t.Parallel()
+	for i := 0; i < 100; i += 2 {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !even.IsEvenCache(i) {
+				t.Error(false)
+			}
+		})
+	}
+}
+
+func TestIsEvenCacheReturningFalseForOddNumbers(t *testing.T) {
+	t.Parallel()
+	for i := 1; i < 100; i += 2 {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if even.IsEvenCache(i) {
+				t.Error(true)
+			}
+		})
+	}
+}
